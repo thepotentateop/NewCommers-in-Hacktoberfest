036fb66f6309d945ce41bfaaacabb69a8eda410e
Aman <62813940+king04aman@users.noreply.github.com>
2023-10-30 21:28:50 +0530

    Update setting.py

--- src/setting.py
+++ src/setting.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DISPLAY_NAME = os.getenv('display_name')          " Get actual Name from env 
-SENDER_EMAIL = os.getenv('sender_email')          " Get actual Email from env
-PASSWORD = os.getenv('password')                  " Get actual Password from env
+DISPLAY_NAME = os.getenv('display_name')          # Get actual Name from env 
+SENDER_EMAIL = os.getenv('sender_email')          # Get actual Email from env
+PASSWORD = os.getenv('password')                  # Get actual Password from env
 
 try:
     assert DISPLAY_NAME
